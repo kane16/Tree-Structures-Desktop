692d9e009fa9c20a77ed54a4b98a5eed11e2e164
Łukasz Guminski <lukasz.guminski629@gmail.com>
2021-05-12 14:00:01 +0200

    Draw tree done.

--- src/main/kotlin/common/view/TreeView.kt
+++ src/main/kotlin/common/view/TreeView.kt
@@ -6,23 +6,37 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Paint
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
+import androidx.compose.ui.graphics.nativeCanvas
 import common.model.Tree
+import org.jetbrains.skija.TextLine
+import org.jetbrains.skija.Typeface
+import java.awt.Font
 
 @Composable
 fun createTree(tree: Tree) {
     Canvas(modifier = Modifier.fillMaxSize()){
-        drawLine(Color.Black, Offset(size.width / 2, 200f), Offset(size.width / 2 - 40f, 230f))
+        drawLine(Color.Black, Offset(size.width / 2, 200f), Offset(size.width / 2 - 300f, 300f))
         drawCircle(
             Color.White,
             center = Offset(size.width / 2, 200f),
-            radius = 10f
+            radius = 40f
         )
         drawCircle(
             Color.White,
-            center = Offset(size.width / 2 - 40f, 230f),
-            radius = 10f
+            center = Offset(size.width / 2 - 300f, 300f),
+            radius = 40f
         )
+        val paint = Paint().asFrameworkPaint()
+        paint.apply {
+            isAntiAlias = true
+        }
+
+        drawIntoCanvas {
+            it.nativeCanvas.drawTextLine(TextLine.make("22", org.jetbrains.skija.Font(Typeface.makeDefault(), 40f)), size.width / 2 - 20f, 200f + 12f, paint)
+            it.nativeCanvas.drawTextLine(TextLine.make("1", org.jetbrains.skija.Font(Typeface.makeDefault(), 40f)), size.width / 2 - 310f, 300f + 12f, paint)
+        }
     }
 }
